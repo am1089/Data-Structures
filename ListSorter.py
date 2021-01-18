f30d043fe211568c06124db29517748530d2ae75
am1089 <adityamitra1089@gmail.com>
2021-01-18 15:31:37 -0800

    Made code faster by making it not go to the end of the list everytime

--- ListSorter.py
+++ ListSorter.py
@@ -1,19 +1,30 @@
 import random
 import time
 
-def sort():
+size = int(input('How big is the list: '))
+RANGE = int(input('What will be the max value: '))
+numbers = []
+switch = []
+for i in range(0, size):
+    n = random.randint(1, RANGE)
+    numbers.append(n)
+    switch.append(n)
+
+def sort(numbers, switch):
+    p = len(numbers)
     starttime = time.time()
-    size = int(input('How big is the list: '))
-    RANGE = int(input('What will be the max value: '))
-    numbers = []
-    for i in range(0, size):
-        n = random.randint(1, RANGE)
-        numbers.append(n)
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
+    k = 1
+    for i in range(p - k):
+        for j in range(i + 1, p):
             if numbers[i] > numbers[j]:
-             numbers[i], numbers[j] = numbers[j], numbers[i]
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+                k += 1
+        if switch == numbers:
+            break
+        else:
+            switch = numbers.copy()
+            continue    
     print(numbers)
     print(time.time() - starttime)
 
-sort()
+sort(numbers, switch)
