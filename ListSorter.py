0eabf7fa44545d2c4b7a7bef45abe38ca15344ab
am1089 <adityamitra1089@gmail.com>
2021-02-21 14:35:56 -0800

    Added quick sort method

--- ListSorter.py
+++ ListSorter.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 import time
 
 size = int(input('How big is the list: '))
@@ -10,21 +11,46 @@ for i in range(0, size):
     numbers.append(n)
     switch.append(n)
 
-def sort(numbers, switch):
-    p = len(numbers)
+def bubblesort(array, switch):
+    p = len(array)
     starttime = time.time()
     k = 1
     for i in range(p - k):
         for j in range(i + 1, p):
-            if numbers[i] > numbers[j]:
-                numbers[i], numbers[j] = numbers[j], numbers[i]
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
                 k += 1
-        if switch == numbers:
+        if switch == array:
             break
         else:
-            switch = numbers.copy()
-            continue    
-    print(numbers)
-    print(time.time() - starttime)
+            switch = array.copy()
+            continue   
+    endtime = time.time() - starttime
+    print(array)
+    print(endtime)
 
-sort(numbers, switch)
+def quicksort(array):
+    starttime = time.time()
+    if len(array) <= 1:
+        return array
+
+    left  = []
+    right = []
+    equal = []
+    pivot = array[0]
+    for num in array:
+        if num < pivot:
+            left.append(num)
+        elif num == pivot:
+            equal.append(num)
+        else:
+            right.append(num)
+
+    leftside = quicksort(left)
+    rightside = quicksort(right)
+    finallist = leftside + equal + rightside 
+    endtime = time.time() - starttime
+    return finallist
+
+print(quicksort(numbers))
+
